2f62867e4f3a317e7070ba5b9ea51bfa686d5dd6
thilakshi <it19971940@my.sliit.lk>
2024-07-06 14:00:04 +0530

    Add project files.

---
+++ AmusedRestApiTest/AmuTestOne.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+using Xunit;
+using FluentAssertions;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace AmusedRestApiTest
+{
+    public class AmuTestOne
+    {
+        private const string BaseUrl = "https://api.restful-api.dev";
+
+        //Test Case 01 - Get List Of All Objects
+        [Fact]
+        public async Task GetListOfAllObjects_ShouldReturnAllObjects()
+        {
+            // Arrange
+            var client = new RestClient(BaseUrl);
+            var request = new RestRequest("objects", Method.Get);
+
+            // Act
+            var response = await client.ExecuteAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            var content = response.Content;
+
+        }
+
+        //Test Case 02 - Get List of objects by its id
+        [Fact]
+        public async Task GetListOfAllObjectsById_ShouldReturnObjectById()
+        {
+            // Arrange
+            var client = new RestClient(BaseUrl);
+            var request = new RestRequest("objects?id=3&id=5&id=10", Method.Get);
+
+            // Act
+            var response = await client.ExecuteAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            var content = response.Content;
+        }
+
+        //Test Case 03 - Retreive Single Object using Id
+        [Fact]
+        public async Task RetrieveSingleObject_ShouldRetrieveSingleObject()
+        {
+            // Arrange
+            var client = new RestClient(BaseUrl);
+            var request = new RestRequest("objects/7", Method.Get);
+
+            // Act
+            var response = await client.ExecuteAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            var content = response.Content;
+        } 
+
+        //Test Case 04 - Add New Object
+        [Fact]
+        public async Task AddNewObject_ShouldAddNewObject()
+        {
+            // Arrange
+            var client = new RestClient(BaseUrl);
+            var request = new RestRequest("objects", Method.Post);
+            request.AddJsonBody(new { name = "Apple MacBook Pro 16", year = "2019", price= "1849.99",
+                CPUmodel= "Intel Core i9", Harddisksize = "1 TB"});
+
+            // Act
+            var response = await client.ExecuteAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            var content = response.Content;
+        }
+
+        //Test Case 05 - Update object
+        [Fact]
+        public async Task UpdateObject_ShouldReturnUpdatedObject()
+        {
+            // Arrange
+            var client = new RestClient(BaseUrl);
+            var request = new RestRequest("objects/7", Method.Put);
+            request.AddJsonBody(new {
+                name = "Apple MacBook Pro 16",
+                year = "2019",
+                price = "2049.99",
+                CPUmodel = "Intel Core i9",
+                Harddisksize = "1 TB",
+                color = "silver"
+            });
+
+            // Act
+            var response = await client.ExecuteAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            var content = response.Content;
+        } 
+
+        //Test case 06 - Partially Update Object
+        [Fact]
+        public async Task UpdateObjectPartially_ShouldReturnUpdatedObjectPartially()
+        {
+            // Arrange
+            var client = new RestClient(BaseUrl);
+            var request = new RestRequest("objects/7", Method.Patch);
+            request.AddJsonBody(new{name = "Apple MacBook Pro 16 (Updated name)"});
+
+            // Act
+            var response = await client.ExecuteAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            var content = response.Content;
+        }
+
+        //Test Case 07 - Delete Object
+        [Fact]
+        public async Task DeleteObject_ShouldReturnNoContent()
+        {
+            // Arrange
+            var client = new RestClient(BaseUrl);
+            var request = new RestRequest("objects/6", Method.Delete);
+
+            // Act
+            var response = await client.ExecuteAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.NoContent);
+        }
+
+
+
+    }
+}
---
+++ Readme.txt.txt
@@ -0,0 +1,23 @@
+How to Run The tests.
+
+Versions 
+Visual Studio - Microsoft Visual Studio Community 2022
+Dotnet Version - 8.0.302
+c# version - 12.0
+
+Step 1
+Open the project and add the NuGet packages RestSharp,Xunit and FluentAssertions
+
+Step 2
+Build the project - Build > Build Solution
+
+Step 3
+Run the test
+Test > Test Explorer > Run
+
+For code Verifications please navigate to AmuTestOne.cs
+
+Obtained Results
+No of Executed tests - 08
+No of Passed tests - 05
+No of failed tests - 03
